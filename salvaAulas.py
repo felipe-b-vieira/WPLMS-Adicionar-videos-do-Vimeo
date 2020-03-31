80017043c3c46fb4e2edf30fb3905c53c6bfa63a
Felipe Vieira <felipe_brito@id.uff.br>
2020-03-30 23:01:17 -0300

    Agora adiciona no curso especificado

--- salvaAulas.py
+++ salvaAulas.py
@@ -4,15 +4,18 @@ import base64
 
 #abre os arquivos com informações de videos e do usuario, a primeira linha é só infos do usuario
 nomeArquivo = input("Por favor, entre com o nome do arquivo:\n")
-arq = open(nomeArquivo)
+arq = open(nomeArquivo, encoding="utf8")
 infosSistema = arq.readline().split(',')
 
 #4 infos genericas para salvar as aulas, usuario, senha do app, url para as requests api no site, e código do autor
-user = infosSistema[0]
+user = 'admin'
 pythonapp = infosSistema[1]
 url = infosSistema[2]
 autor = int(infosSistema[3])
+cursoId = int(infosSistema[4])
+cursoId = str(cursoId)
 
+print(user)
 #header do request
 token = base64.standard_b64encode(str(user + ':' + pythonapp).encode('ascii'))
 headers = {'Authorization': 'Basic ' + token.decode('utf-8'),
@@ -20,6 +23,12 @@ headers = {'Authorization': 'Basic ' + token.decode('utf-8'),
 'User-Agent':'Mozilla',
 'Accept':'*/*',
 }
+
+#pega as infos do curso, curriculo
+courses = requests.get(url + 'course/'+cursoId,headers=headers)
+cJson = courses.json()
+curriculum = cJson['vibe_course_curriculum']
+
 
 for aula in arq:
 	infosAula = aula.split(',')
@@ -36,5 +45,16 @@ for aula in arq:
 			'vibe_duration' : infosAula[1],
 			}
 	#faz o request real e adiciona a aula no site.
-	r = requests.post(url + '/unit/', headers=headers, json=post)
+	print(url + 'units/')
+	r = requests.post(url + 'unit/', headers=headers, json=post)
 	print('Your post is published on ' + str(r.content))
+	rJson = r.json()
+	curriculum.append(rJson['id'])
+
+
+
+post={
+	'vibe_course_curriculum':curriculum,
+}
+r = requests.post(url + 'course/'+cursoId, headers=headers, json=post)
+print('Your post is published on ' + str(r.content))
